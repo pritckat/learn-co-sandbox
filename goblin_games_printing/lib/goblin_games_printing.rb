ba9721dfb0a57a7810aba39f3cf6e6127e363352
Katrina Pritchard <pritckat@gmail.com>
2020-04-27 20:24:05 +0000

    test

---
+++ goblin_games_printing/lib/goblin_games_printing.rb
@@ -0,0 +1,6 @@
+require "goblin_games_printing/version"
+
+module GoblinGamesPrinting
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ goblin_games_printing/lib/goblin_games_printing/version.rb
@@ -0,0 +1,3 @@
+module GoblinGamesPrinting
+  VERSION = "0.1.0"
+end
